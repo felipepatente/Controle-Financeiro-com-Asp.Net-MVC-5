2dc126af4b9c93c88003cf5717c216e5db277601
felipepatente <felipepatente@yahoo.com.br>
2018-12-13 08:35:42 -0200

    Cadastro de Movimentações

--- Financas/Financas/Entidades/Movimentacao.cs
+++ Financas/Financas/Entidades/Movimentacao.cs
@@ -17,7 +17,7 @@ namespace Financas.Entidades
 
         public int UsuarioId { get; set; }
 
-        public Usuario Usuario { get; set; }
+        public virtual Usuario Usuario { get; set; }
 
 
     }
